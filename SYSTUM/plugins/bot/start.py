ca87eaed0dfcacb03a9e1782b8de1e5313a724bd
sielzz <93588547+sielzz@users.noreply.github.com>
2024-12-07 15:14:30 +0700

    start.py

--- SYSTUM/plugins/bot/start.py
+++ SYSTUM/plugins/bot/start.py
@@ -46,7 +46,7 @@ async def start_pm(client, message: Message, _):
                 )
             return
         if name[0:3] == "inf":
-            m = await message.reply_text("🔎")
+            m = await message.reply_text("SABAR JING🔎")
             query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
